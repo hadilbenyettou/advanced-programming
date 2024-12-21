7aa419a5afd982d77bbfaf3b90e165ff51e65b35
hadilbenyettou <hben7065@gmail.com>
2024-12-21 19:18:00 +0100

    exo24

---
+++ exo24/arguments.py
@@ -0,0 +1,9 @@
+def anagrams(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+
+print(anagrams("tame", "meta"))
+print(anagrams("tame", "mate"))
+print(anagrams("tame", "team"))
+print(anagrams("tabby", "batty"))
+print(anagrams("python", "java"))
